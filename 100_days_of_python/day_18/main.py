20511c04452360138d7ed4af2a9f46cf82fc9b8d
Greg Vargas <gvarg75@gmail.com>
2021-10-11 10:37:19 -0700

    updated python to fix turtle issue

--- 100_days_of_python/day_18/main.py
+++ 100_days_of_python/day_18/main.py
@@ -1,6 +1,13 @@
-from turtle import Turtle, Screen
+from turtle import *
 
-timmy = Turtle()
+tim = Turtle()
 
 screen = Screen()
 screen.exitonclick()
+
+from turtle import Turtle
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
